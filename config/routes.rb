f581881e48a6016310535c49ab451bf8a4de1df6
bachhx-0399 <hoang.xuan.bach@sun-asterisk.com>
2023-08-23 11:39:20 +0700

    Chapter 12: Password Reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
   scope "(:locale)", locale: /en|vi/ do
     root "static_pages#home"
     get  "/help",    to: "static_pages#help"
@@ -11,6 +10,7 @@ Rails.application.routes.draw do
     delete "/logout",  to: "sessions#destroy"
     resources :users
     resources :account_activations, only: :edit
+    resources :password_resets, only: %i(new create edit update)
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
